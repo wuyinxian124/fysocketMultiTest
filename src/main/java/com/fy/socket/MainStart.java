8cecb941dfbcfafa887e2555580ac623de01e1ca
wuyinxian124 <wuyinxian124@gmail.com>
2015-07-02 15:39:54 +0800

    添加参数说明

--- src/main/java/com/fy/socket/MainStart.java
+++ src/main/java/com/fy/socket/MainStart.java
@@ -6,6 +6,10 @@ public class MainStart {
 
 	public static void main(String[] args) {
 
+		System.out.println("0 -99 用户，互动室 /10 ,最大id9");
+		System.out.println("100 -199 用户，互动室 /10 ,最大id19");
+		System.out.println("900 -999 用户，互动室 /10 ,最大id99");
+		System.out.println("1000 -1009 用户，互动室 /10 ,最大id100");
 		// 使用appCient 测试
 		new MultMain().start(args);
 		
